f41e96d480813e91d631f5fab5fc5cdbf0028b18
SherryMagic <sherryliang6266@gmail.com>
2026-02-19 23:39:07 -0700

    Modified item card in items folder.

--- app/Items/commonitemslist.tsx
+++ app/Items/commonitemslist.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
-import ItemCard from "../component/itemCard";
+import ItemCard from "./itemCard";
 
 const commonItems = [
   {
